00dc1650fc1542ede80cc4721c767f9927367c04
annahluiza <ana.barbosa.lopes@escola.pr.gov.br>
2025-09-24 09:32:37 -0300

    a

--- script.js
+++ script.js
@@ -12,33 +12,32 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto) { 
-    let palavras= texto.splint(/\P{L}+/u);
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
 
-    for(let i in palavras)
-        palavras [i] = palavras[i].toLowerCase();
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    return ordenadas.slice(0, 10);
 }
 
-palavras = tiraPalavrasRuins(palavras);
-
-const frequencias = comtaFrenquicia(palavras);
-let ordenadas = Object.keys(frequencias).sprt(ordenaPalavra);
-
-function ordenaPalavras(p1,p2){
-    return frenquecias[p2]-frequencias[p2];
-}
-
-return ordenadas.slice(0,10);
-
-  
-
-function comtaFrequencia(palavras){
-    let frequencias={};
-    for(let i of palavras) {
-        frequencias[i]=0;
-        for(let j of palavras) {
-            if (i==j) {
-                frequencias[i] ++;
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
             }
         }
     }
@@ -46,9 +45,9 @@ function comtaFrequencia(palavras){
 }
 
 function tiraPalavrasRuins(palavras) {
-    conts palavrasBoas = [];
-    for(let palavras of palavras) {
-        if(!palavras of palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
             palavrasBoas.push(palavra);
         }
     }
